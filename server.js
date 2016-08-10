94dde088c37f258d902a3bd4223c4eacdd253df8
anh-nguyen <anh-nguyen@cybozu.vn>
2016-08-10 19:40:40 +0700

    test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-var server = app.listen(80 || 8080, function () {
+var server = app.listen(process.env.PORT || 8080, function () {
 });
 
 app.post("/contacts", function(req, res) {
